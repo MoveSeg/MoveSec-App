13484db34704a7b6f7cd64c7edddb5b37a3397e4
Alana <116409209+fralana@users.noreply.github.com>
2024-02-28 14:38:26 -0300

    problemas resolvidos

--- src/main/java/com/moveseg/app/cadastro/responsavel/infra/EmailValidator.java
+++ src/main/java/com/moveseg/app/cadastro/responsavel/infra/EmailValidator.java
@@ -7,11 +7,11 @@ public class EmailValidator implements ConstraintValidator<EmailAnnotation, Stri
 
     @Override
     public boolean isValid(String email, ConstraintValidatorContext context) {
-        email = email.trim();
 
-        if (email.matches(".*[a-zA-Z0-9]+.*@.*[a-zA-Z0-9]+.*")) {
-            return email.matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
-        }
-        return false;
+        String emailAlfanumerico = email.replaceAll(".*[a-zA-Z0-9]+.*@.*[a-zA-Z0-9]+.*", "");
+
+        return emailAlfanumerico
+                .matches("^[a-zA-Z0-9_+&*-]+(?:\\.[a-zA-Z0-9_+&*-]+)*@(?:[a-zA-Z0-9-]+\\.)+[a-zA-Z]{2,7}$");
+
     }
 }
