88d545495445dff5be8d5a4367d735734fa36e4d
Álvaro Augusto Zago <43797853+AlvaroAugustoZago@users.noreply.github.com>
2024-03-15 14:57:57 -0300

    fix: ajustes

--- src/test/java/com/moveseg/app/cadastro/Veiculo/domain/PlacaTest.java
+++ src/test/java/com/moveseg/app/cadastro/Veiculo/domain/PlacaTest.java
@@ -1,12 +1,10 @@
-package com.moveseg.app.cadastro.Veiculo.domain;
+package com.moveseg.app.cadastro.veiculo.domain;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
-
-import com.moveseg.app.cadastro.veiculo.domain.Placa;
 
 class PlacaTest {
 
