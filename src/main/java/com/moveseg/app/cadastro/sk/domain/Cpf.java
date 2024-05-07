3eeb79806a03d5f798f21bd27446069cdb505c0b
Alana <116409209+fralana@users.noreply.github.com>
2024-05-07 15:17:55 -0300

    fix: Cpf

--- src/main/java/com/moveseg/app/cadastro/sk/domain/Cpf.java
+++ src/main/java/com/moveseg/app/cadastro/sk/domain/Cpf.java
@@ -18,7 +18,7 @@ public class Cpf implements ValueObject {
     private String numero;
 
     public static Cpf of(String numero) throws Exception{
-        if (numero == null || numero.isEmpty()) {
+        if(numero == null || numero.isEmpty()) {
             throw new Exception("O seu CPF não pode ser nulo e nem 0");
         }
         return new Cpf(numero);
