98e93427d49917c3cb21b97602dadcb0d059906b
Guilherme-Cardozo-688 <155786316+Guilherme-Cardozo-688@users.noreply.github.com>
2024-06-26 17:35:38 -0300

    fix: testes de programacao

--- src/test/java/com/moveseg/app/viagem/Programacao/domain/ProgramacaoTest.java
+++ src/test/java/com/moveseg/app/viagem/Programacao/domain/ProgramacaoTest.java
@@ -5,20 +5,40 @@ import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import com.moveseg.app.cadastro.Aluno.domain.Aluno;
+import com.moveseg.app.cadastro.Motorista.domain.MotoristaId;
+import com.moveseg.app.cadastro.veiculo.domain.VeiculoId;
+import com.moveseg.app.cadastro.veiculo.sk.domain.Genero;
+import com.moveseg.app.viagem.Rota.domain.RotaId;
 import com.moveseg.app.viagem.domain.Viagem;
 import com.moveseg.app.viagem.domain.Viagem.ViagemBuilder;
+import com.moveseg.parent.infra.domain.DomainObjectId;
 
 public class ProgramacaoTest {
-    private ViagemBuilder builder;
-
-    private Viagem viagem;
+    private Aluno aluno;
+    private List<Aluno> alunos = new ArrayList<Aluno>();
+    private RotaId rota = DomainObjectId.randomId(RotaId.class);
+    private MotoristaId motorista = DomainObjectId.randomId(MotoristaId.class);
+    private VeiculoId veiculo = DomainObjectId.randomId(VeiculoId.class);
+    private LocalDate dataViagem = LocalDate.of(2000, 04, 30);
     private LocalDate data;
+    private Viagem viagem;
 
     @Test
     void agendaCompletoDeveSalvar() {
+    this.alunos.add(aluno);
+    viagem = Viagem.builder()
+    .alunos(alunos)
+    .motorista(motorista)
+    .rota(rota)
+    .veiculo(veiculo)
+    .data(dataViagem)
+    .build();
         data = LocalDate.of(2000, 1, 20);
         Programacao programacao = Programacao.from(viagem, data);
         assertNotNull(programacao);
@@ -29,6 +49,14 @@ public class ProgramacaoTest {
 
     @Test
     void dadoUmaAgendaSemDataNaoDeveCriar() {
+        this.alunos.add(aluno);
+        viagem = Viagem.builder()
+        .alunos(alunos)
+        .motorista(motorista)
+        .rota(rota)
+        .veiculo(veiculo)
+        .data(dataViagem)
+        .build();
         assertThrows(Exception.class, () -> {
             Programacao.from(viagem, null);
         });
