be8c95c39620b0dda074b00f09088d6b455082d7
Guilherme-Cardozo-688 <155786316+Guilherme-Cardozo-688@users.noreply.github.com>
2024-08-02 14:48:50 -0300

    Correção de Id aluno

--- src/main/java/com/moveseg/app/cadastro/Aluno/domain/AlunoId.java
+++ src/main/java/com/moveseg/app/cadastro/Aluno/domain/AlunoId.java
@@ -1,12 +1,13 @@
 package com.moveseg.app.cadastro.Aluno.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.moveseg.parent.infra.domain.DomainObjectId;
 
 public class AlunoId extends DomainObjectId{
- @JsonCreator
-    public AlunoId(@JsonProperty("aluno") String uuid){
+    //@JsonCreator
+    //public AlunoId(@JsonProperty("aluno") String uuid){
+      //  super(uuid);
+    //}
+    public AlunoId(String uuid) {
         super(uuid);
     }
 
