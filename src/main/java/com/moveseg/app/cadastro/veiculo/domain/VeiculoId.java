0eb03f2677fe5a7c95a857b606f25d258dfc2d96
Guilherme-Cardozo-688 <155786316+Guilherme-Cardozo-688@users.noreply.github.com>
2024-08-07 15:40:22 -0300

    fix: mudança de alunos no alterar viagem

--- src/main/java/com/moveseg/app/cadastro/veiculo/domain/VeiculoId.java
+++ src/main/java/com/moveseg/app/cadastro/veiculo/domain/VeiculoId.java
@@ -1,12 +1,9 @@
 package com.moveseg.app.cadastro.veiculo.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonProperty;
 import com.moveseg.parent.infra.domain.DomainObjectId;
 
 public class VeiculoId extends DomainObjectId {
- @JsonCreator
-    public VeiculoId(@JsonProperty("veiculo") String uuid) {
+    public VeiculoId(String uuid) {
         super(uuid);
     }
 }
--- src/main/java/com/moveseg/app/viagem/domain/cmd/AlterarViagem.java
+++ src/main/java/com/moveseg/app/viagem/domain/cmd/AlterarViagem.java
@@ -3,7 +3,6 @@ package com.moveseg.app.viagem.domain.cmd;
 import java.time.LocalDate;
 import java.util.List;
 
-import com.moveseg.app.cadastro.Aluno.domain.Aluno;
 import com.moveseg.app.cadastro.Motorista.domain.MotoristaId;
 import com.moveseg.app.cadastro.veiculo.domain.VeiculoId;
 import com.moveseg.app.viagem.Rota.domain.RotaId;
@@ -16,7 +15,7 @@ import lombok.Data;
 @Builder
 public class AlterarViagem {
     private ViagemId id;
-    private List<Aluno> alunos;
+    private List<String> alunos;
     private RotaId rota;
     private MotoristaId motorista;
     private VeiculoId veiculo;
