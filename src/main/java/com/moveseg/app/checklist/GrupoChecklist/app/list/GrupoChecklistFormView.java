b086f05fee8eb1375b11c1ebe92822d00639a1da
Guilherme-Cardozo-688 <155786316+Guilherme-Cardozo-688@users.noreply.github.com>
2024-09-11 19:31:56 -0300

    bug fix(grupoChecklist): Corrige conflitos de merge no `GrupoChecklistFormView`

--- src/main/java/com/moveseg/app/checklist/GrupoChecklist/app/list/GrupoChecklistFormView.java
+++ src/main/java/com/moveseg/app/checklist/GrupoChecklist/app/list/GrupoChecklistFormView.java
@@ -3,12 +3,7 @@ package com.moveseg.app.checklist.GrupoChecklist.app.list;
 import java.util.List;
 import java.util.stream.Collectors;
 
-<<<<<<< Updated upstream
 import com.moveseg.app.checklist.Checklist.app.view.ChecklistListView;
-=======
-import com.moveseg.app.checklist.Checklist.domain.Checklist;
-import com.moveseg.app.checklist.Checklist.domain.Nome;
->>>>>>> Stashed changes
 import com.moveseg.app.checklist.GrupoChecklist.domain.GrupoChecklist;
 import com.moveseg.app.checklist.GrupoChecklist.domain.GrupoChecklistId;
 
@@ -24,23 +19,15 @@ import lombok.NoArgsConstructor;
 public class GrupoChecklistFormView {
 private GrupoChecklistId id;
     private String nome;
-<<<<<<< Updated upstream
     private List<ChecklistListView> checklists;
-=======
-    private List<Nome> checklists;
->>>>>>> Stashed changes
     
     public static GrupoChecklistFormView of(GrupoChecklist grupoChecklist) {
         return GrupoChecklistFormView.builder()
                 .id(grupoChecklist.id())
                 .nome(grupoChecklist.nome().nome())
-<<<<<<< Updated upstream
                 .checklists(grupoChecklist.checklists().stream()
                         .map(ChecklistListView::of)
                         .collect(Collectors.toList()))
-=======
-                .checklists(grupoChecklist.checklists().stream().map(Checklist::nome).collect(Collectors.toList()))
->>>>>>> Stashed changes
                 .build();
     }
 }
